d9ce2dcca0bbf39eff8764d0d6c67ab624aa93be
jdlrobson <jdlrobson@gmail.com>
2016-07-06 15:40:56 -0700

    Correct typo in WikiSocketCollection options

--- libs/server/app.js
+++ libs/server/app.js
@@ -18,7 +18,7 @@ const collection = new WikiSocketCollection( {
   id: 'mysocket',
   project: '*.wikipedia.org',
   minPurgeTime: 20,
-  maxLifeSpan: ( 60 * 24 ) * 7,
+  maxLifespan: ( 60 * 24 ) * 7,
   maxInactivity: ( 60 * 24 ) * 7,
   minSpeed: 0.1
 } );
