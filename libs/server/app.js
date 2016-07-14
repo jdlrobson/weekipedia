aca07b3ff7975bc6ff4fab4149d7a45e3db6ffaa
jdlrobson <jdlrobson@gmail.com>
2016-07-14 06:31:12 -0700

    Split out trending endpoint from god app.js file
    In order to make app more manageable we should split all the endpoints
    into separate files commencing with the trending endpoint
    
    Additional changes:
    * Timeframe replaced with more appropiate halflife name

--- libs/server/app.js
+++ libs/server/app.js
@@ -3,9 +3,9 @@ require('babel-core/register')
 import express from 'express'
 import hogan from 'hogan-express'
 import NodeCache from 'node-cache';
-import WikiSocketCollection from 'WikiSocketCollection'
 import fetch from 'isomorphic-fetch'
-import addProps from './prop-enricher'
+
+import trending from './endpoints/trending'
 
 // Express
 const app = express()
@@ -15,96 +15,30 @@ app.use('/', express.static(__dirname + '/../../public/'))
 app.set('port', (process.env.PORT || 3000))
 
 const shortLifeCache = new NodeCache( { stdTTL: 60 * 10, checkperiod: 60 * 10 } );
-const collection = new WikiSocketCollection( {
-  id: 'mysocket',
-  project: '*.wikipedia.org',
-  minPurgeTime: 20,
-  maxLifespan: ( 60 * 24 ) * 7,
-  maxInactivity: ( 60 * 24 ) * 7,
-  minSpeed: 0.1
-} );
 
-function calcScore( q, hrs ) {
-  return ( ( q.edits - q.anonEdits - q.reverts ) + ( q.anonEdits * 0.2 ) )
-    / q.getBias()
-    * ( q.contributors.length / 2 )
-    * Math.pow(0.5, q.age() / (hrs * 60));
-}
-
-function getSortedPages( hrs ) {
-  // FIXME: This should be cached for a fixed window e.g. 5 minutes?
-  var p = collection.getPages();
-  return p.sort( function ( q, r ) {
-    return calcScore( q, hrs ) > calcScore( r, hrs ) ? -1 : 1;
-  } );
-}
-
-function annotate( p, filter, limit ) {
-  var res = [];
-  p.some( function ( item, i ) {
-    if ( !item.wiki ) {
-      item.wiki = 'enwiki';
-    }
-    if ( res.length >= limit ) {
-      return true;
-    } else if ( filter && filter( item ) ) {
-      var score =  calcScore( item );
-      var speed = item.editsPerMinute();
-
-      item.lastIndex = item.index ? item.index : limit;
-      item.index = res.length + 1;
-      if ( !item.bestIndex ) {
-        item.bestIndex = item.index;
-      } else if ( item.index < item.bestIndex ) {
-        item.bestIndex = item.index;
-      }
-      item.bias = item.getBias();
-      item.score = score;
-      // Mark trending ones as safe until end of lifespan
-      // Note: a speed of 0.2 after 5 minutes is equivalent to 1 edit every minute.
-      if ( speed > 0.2 && item.bias < 0.75 && item.age() > 5 && item.contributors.length > 2 ) {
-        console.log( 'Marked', item.title, 'as safe');
-        collection.markSafe( item.id );
-      }
-      res.push( item );
-    }
-  } );
-  return res;
-}
-
-app.get('/api/trending/:wiki?/:timeframe?',(req, res) => {
-  var fn, results;
+app.get('/api/trending/:wiki?/:halflife?',(req, res) => {
   var wiki = req.params.wiki || 'enwiki';
-  var timeframe = parseFloat(req.params.timeframe) || 5;
-  var cacheKey = 'trending/' + wiki + '/' + timeframe;
+  var halflife = parseFloat( req.params.halflife ) || 5;
+  var cacheKey = 'trending/' + wiki + '/' + halflife;
 
   res.setHeader('Content-Type', 'application/json');
 
   shortLifeCache.get( cacheKey, function( err, responseText ) {
     var responseText;
     if ( err || !responseText ) {
-      fn = function ( item ) {
-        return item.contributors.length > 1 && ( wiki === '*' || item.wiki === wiki );
-      };
-
-      results = annotate( getSortedPages(timeframe), fn, 50 );
-      if ( !results.length ) {
-        res.status(404);
+      trending( wiki, halflife ).then( function ( data ) {
+        responseText = JSON.stringify( data );
+        shortLifeCache.set( cacheKey, responseText );
+        res.status( 200 );
+        res.send( responseText );
+      }).catch( function () {
+        res.status( 404 );
         res.send( JSON.stringify( {
           msg: 'No results'
         } ) );
-      } else {
-        addProps(results, ['pageimages','pageterms']).then(function(results) {
-          responseText = JSON.stringify( {
-            results: results, ts: new Date()
-          } );
-          shortLifeCache.set( cacheKey, responseText );
-          res.status(200);
-          res.send( responseText );
-        })
-      }
+      });
     } else {
-      res.status(200);
+      res.status( 200 );
       res.send( responseText );
     }
   } );
---
+++ libs/server/endpoints/trending.js
@@ -0,0 +1,85 @@
+import WikiSocketCollection from 'WikiSocketCollection'
+import fetch from 'isomorphic-fetch'
+
+import addProps from './../prop-enricher'
+
+const collection = new WikiSocketCollection( {
+  id: 'mysocket',
+  project: '*.wikipedia.org',
+  minPurgeTime: 20,
+  maxLifespan: ( 60 * 24 ) * 7,
+  maxInactivity: ( 60 * 24 ) * 7,
+  minSpeed: 0.1
+} );
+
+function calcScore( q, hrs ) {
+  return ( ( q.edits - q.anonEdits - q.reverts ) + ( q.anonEdits * 0.2 ) )
+    / q.getBias()
+    * ( q.contributors.length / 2 )
+    * Math.pow( 0.5, q.age() / ( hrs * 60 ) );
+}
+
+function getSortedPages( hrs ) {
+  // FIXME: This should be cached for a fixed window e.g. 5 minutes?
+  var p = collection.getPages();
+  return p.sort( function ( q, r ) {
+    return calcScore( q, hrs ) > calcScore( r, hrs ) ? -1 : 1;
+  } );
+}
+
+function annotate( p, filter, limit ) {
+  var res = [];
+  p.some( function ( item, i ) {
+    if ( !item.wiki ) {
+      item.wiki = 'enwiki';
+    }
+    if ( res.length >= limit ) {
+      return true;
+    } else if ( filter && filter( item ) ) {
+      var score =  calcScore( item );
+      var speed = item.editsPerMinute();
+
+      item.lastIndex = item.index ? item.index : limit;
+      item.index = res.length + 1;
+      if ( !item.bestIndex ) {
+        item.bestIndex = item.index;
+      } else if ( item.index < item.bestIndex ) {
+        item.bestIndex = item.index;
+      }
+      item.bias = item.getBias();
+      item.score = score;
+      // Mark trending ones as safe until end of lifespan
+      // Note: a speed of 0.2 after 5 minutes is equivalent to 1 edit every minute.
+      if ( speed > 0.2 && item.bias < 0.75 && item.age() > 5 && item.contributors.length > 2 ) {
+        console.log( 'Marked', item.title, 'as safe');
+        collection.markSafe( item.id );
+      }
+      res.push( item );
+    }
+  } );
+  return res;
+}
+
+/**
+ * @param {String} wiki name of wiki to generate a list of trending articles for
+ * @param {Float} halflife in hours at which pages become less trending
+ */
+function trending( wiki, halflife ) {
+  return new Promise( function ( resolve, reject ) {
+    var fn = function ( item ) {
+      return item.contributors.length > 1 && ( wiki === '*' || item.wiki === wiki );
+    };
+    var results = annotate( getSortedPages( halflife ), fn, 50 );
+    if ( !results.length ) {
+      reject();
+    } else {
+      addProps( results, [ 'pageimages','pageterms' ] ).then( function( results ) {
+        resolve( {
+          results: results, ts: new Date()
+        } );
+      })
+    }
+  })
+}
+
+export default trending
