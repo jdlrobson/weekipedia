94e0f4dd63e5389b4cd4f82ef5293e69975a0061
jdlrobson <jdlrobson@gmail.com>
2016-07-13 17:14:57 -0700

    Add license with footer information
    Fixes: #11

--- libs/client/containers/Article/index.jsx
+++ libs/client/containers/Article/index.jsx
@@ -8,6 +8,10 @@ class Article extends Component {
     var header = this.props.title ?
       [ <h1 key="article-title"
         id="section_0" dangerouslySetInnerHTML={{ __html: this.props.title}}></h1> ] : [],
+      license = {
+        url: '//creativecommons.org/licenses/by-sa/3.0/',
+        name: 'CC BY-SA 3.0'
+      },
       tagline = this.props.tagline;
 
     if ( tagline ) {
@@ -22,6 +26,12 @@ class Article extends Component {
           {header}
         </Content>
         {this.props.children}
+        <Content key="footer" className="post-content footer">
+          <ul className="footer-info">
+            <li><h2>Weekipedia</h2></li>
+            <li>Content is available under <a className="external" rel="nofollow" href={license.url}>{license.name}</a> unless otherwise noted.</li>
+          </ul>
+        </Content>
       </div>
     )
   }
