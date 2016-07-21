ecb4a22bea49633815c810ca7d1002b75cb30038
jdlrobson <jdlrobson@gmail.com>
2016-07-21 09:52:42 -0700

    When you don't pass a key to cached response do not cache
    Allow uncached responses. This will be needed for a random api endpoint

--- libs/server/cached-response.js
+++ libs/server/cached-response.js
@@ -2,27 +2,38 @@ import NodeCache from 'node-cache';
 
 const shortLifeCache = new NodeCache( { stdTTL: 60 * 10, checkperiod: 60 * 10 } );
 
+function respond( res, method ) {
+  return method().then( function ( data ) {
+    var responseText = JSON.stringify( data );
+    res.status( 200 );
+    res.send( responseText );
+    return responseText;
+  }).catch( function (a) {
+    res.status( 404 );
+    res.send( JSON.stringify( {
+      msg: '404'
+    } ) );
+  });
+}
+
 function cachedResponse( res, cacheKey, method ){
   res.setHeader('Content-Type', 'application/json');
-  shortLifeCache.get( cacheKey, function( err, responseText ) {
-    var responseText;
-    if ( err || !responseText ) {
-      method().then( function ( data ) {
-        responseText = JSON.stringify( data );
-        shortLifeCache.set( cacheKey, responseText );
+  if ( !cacheKey ) {
+    // no caching requested
+    respond( res, method );
+  } else {
+    shortLifeCache.get( cacheKey, function( err, responseText ) {
+      var responseText;
+      if ( err || !responseText ) {
+        respond( res, method ).then( function ( newResponseText ) {
+          shortLifeCache.set( cacheKey, newResponseText );
+        } );
+      } else {
         res.status( 200 );
         res.send( responseText );
-      }).catch( function () {
-        res.status( 404 );
-        res.send( JSON.stringify( {
-          msg: '404'
-        } ) );
-      });
-    } else {
-      res.status( 200 );
-      res.send( responseText );
-    }
-  } );
+      }
+    } );
+  }
 }
 
 export default cachedResponse
