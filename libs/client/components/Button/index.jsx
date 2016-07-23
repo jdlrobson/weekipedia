f409e6b4366194446b32ed6695c266d2129abd31
jdlrobson <jdlrobson@gmail.com>
2016-07-22 17:47:51 -0700

    Fix primary button implementation
    It was not working correctly.
    Various progressive styles not necessary.

--- libs/client/components/Button/index.jsx
+++ libs/client/components/Button/index.jsx
@@ -2,10 +2,10 @@ import './mediawiki-ui-button.css'
 import React, { Component } from 'react'
 
 const Button = (props) => {
-  var modifiers = props.isPrimary ? 'mw-ui-button-primary' : '';
+  var modifiers = props.isPrimary ? 'mw-ui-primary' : '';
   modifiers += props.isQuiet ? 'mw-ui-quiet' : '';
 
-  return <a className={'mw-ui-button' + modifiers}
+  return <a className={'mw-ui-button ' + modifiers}
     onClick={props.onClick} href={props.href}>{props.label}</a>
 }
 
