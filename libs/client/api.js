2ca8df27809141a096186491b207c41acd03b5cc
jdlrobson <jdlrobson@gmail.com>
2016-07-28 11:42:45 -0700

    Add reference api endpoint
    In preparation for reference drawer.

--- libs/client/api.js
+++ libs/client/api.js
@@ -2,6 +2,7 @@ import fetch from 'isomorphic-fetch'
 
 function Api() {
   this.cache = {};
+  this.refCache = {};
 }
 
 Api.prototype = {
@@ -26,6 +27,47 @@ Api.prototype = {
   getPage: function ( title, lang ) {
     lang = lang || 'en';
     return this.fetch( '/api/page/' + lang + '/' + title );
+  },
+  getReference: function ( title, lang, refId ) {
+    return this.getReferences( title, lang ).then( function ( refs ) {
+      if ( refs[refId] ) {
+        return refs[refId];
+      } else {
+        throw "No reference";
+      }
+    } );
+  },
+  getReferences: function ( title, lang ) {
+    var promise;
+    var reflist = {};
+    var cache = this.refCache;
+    var cacheKey = lang + '#' + title;
+
+    if ( cache[ cacheKey ] ) {
+      return cache[cacheKey];
+    } else {
+      promise = this.getPage( title, lang ).then( function ( json ) {
+        var container = document.createElement( 'div' );
+
+        json.remaining.sections.forEach( function ( section ) {
+          var refNodes;
+          // check it's a reference section
+          if ( section.text.indexOf( 'class="mw-references' ) > -1 ) {
+            container.innerHTML = section.text;
+            refNodes = container.querySelectorAll( '[id]' );
+            Array.prototype.forEach.call( refNodes, function ( refNode ) {
+              var id = refNode.getAttribute( 'id' );
+              if ( id ) {
+                reflist[id] = refNode.innerHTML;
+              }
+            } );
+          }
+        } );
+        return reflist;
+      } );
+      cache[cacheKey] = promise;
+      return promise;
+    }
   },
   getTrending: function ( wiki, halflife ) {
     var url = '/api/trending/';
