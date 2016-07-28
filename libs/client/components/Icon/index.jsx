ac085a5db26418c7279c3e7ef6f49d8354cdf84e
jdlrobson <jdlrobson@gmail.com>
2016-07-28 11:43:34 -0700

    Allow addition of classes to Icon component

--- libs/client/components/Icon/index.jsx
+++ libs/client/components/Icon/index.jsx
@@ -2,7 +2,7 @@ import './mediawiki-ui-icon.css'
 import React, { Component } from 'react'
 
 const Icon = (props) => (
-    <a className={'mw-ui-icon mw-ui-icon-' + props.glyph + ' mw-ui-icon-' + ( props.type || 'element' )}
+    <a className={(props.className || '') + ' mw-ui-icon mw-ui-icon-' + props.glyph + ' mw-ui-icon-' + ( props.type || 'element' )}
       onClick={props.onClick} href={props.href}>{props.label}</a>
   )
 
