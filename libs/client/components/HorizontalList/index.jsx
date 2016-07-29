1d74267c35f0ed62207f4cd09549dac2129e6926
jdlrobson <jdlrobson@gmail.com>
2016-07-29 12:43:07 -0700

    Allow passing of class to HorizontalList

--- libs/client/components/HorizontalList/index.jsx
+++ libs/client/components/HorizontalList/index.jsx
@@ -4,8 +4,9 @@ import './styles.css'
 
 export default React.createClass({
    render: function() {
+     var cl = this.props.className ? this.props.className + ' hlist' : 'hlist';
      return (
-       <ul className={'hlist' + ( this.props.isSeparated ? ' separated' : '')}>
+       <ul className={ cl + ( this.props.isSeparated ? ' separated' : '')}>
          { this.props.children.map( function( child, i ){
            return <li key={'hlist-' + i}>{child}</li>;
          } ) }
