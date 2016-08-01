05bfb01c9658832110a591a392f278a8e7d693ce
jdlrobson <jdlrobson@gmail.com>
2016-08-01 14:12:05 -0700

    Style 404 pages better
    Use ErrorBox and Content container

--- libs/client/router.js
+++ libs/client/router.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import EventEmitter from 'events'
 
+import Content from './containers/Content'
+import ErrorBox from './components/ErrorBox'
+
 var routes = [
   // no fragment
   [
@@ -15,7 +18,9 @@ var routes = [
     function ( info, props ) {
       return Object.assign( {}, props, {
         title: '404 - Page Not Found',
-        children: React.DOM.p({},'The path ' + info[1] + ' is not the path you are looking for.')
+        children: Content({ children:
+          ErrorBox( { msg: 'The path ' + info[1] + ' is not the path you are looking for.' } )
+        })
       } )
     }
   ]
