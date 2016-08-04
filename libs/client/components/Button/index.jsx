63db9128f38b276c47fe983146407e16eabb0854
jdlrobson <jdlrobson@gmail.com>
2016-08-04 11:59:20 -0700

    Allow passing of classes to Button component

--- libs/client/components/Button/index.jsx
+++ libs/client/components/Button/index.jsx
@@ -4,6 +4,7 @@ import React, { Component } from 'react'
 const Button = (props) => {
   var modifiers = props.isPrimary ? 'mw-ui-primary' : '';
   modifiers += props.isQuiet ? 'mw-ui-quiet' : '';
+  modifiers += props.className ? ' ' + props.className : '';
 
   return <a className={'mw-ui-button ' + modifiers}
     onClick={props.onClick} href={props.href}>{props.label}</a>
