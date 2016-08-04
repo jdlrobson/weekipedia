188e33ea386fde21b871cc5b999914e4fe190843
jdlrobson <jdlrobson@gmail.com>
2016-08-04 13:00:32 -0700

    Search form should have a placeholder
    Fixes: #42

--- libs/client/components/SearchForm/index.jsx
+++ libs/client/components/SearchForm/index.jsx
@@ -7,6 +7,7 @@ import './styles.css'
 export default (props) => (
   <form className="search-form">
     <SearchInput onClick={props.onClickSearch} onSearch={props.onSearch}
+      placeholder="Search Weekipedia"
       focusOnRender={props.focusOnRender} />
   </form>
 );
