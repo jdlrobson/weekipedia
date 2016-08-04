61fe0f77d731f7bac986c6523346f878b86c98b4
jdlrobson <jdlrobson@gmail.com>
2016-08-04 14:00:10 -0700

    Introduce section collapsing
    For the time being this behaves differently from English Wikipedia
    There is currently no concept of a subsection. All sections are rendered
    equally so collapsing a h2 will not collapse any h3's underneath it.
    
    Fixes: #40

--- libs/client/components/Section/index.jsx
+++ libs/client/components/Section/index.jsx
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
 
+import Icon from './../Icon'
 import SectionContent from './../SectionContent'
 
+import './styles.css'
+
 class Section extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: true
+    };
+  }
+  onToggle() {
+    this.setState( { isOpen: !this.state.isOpen } );
+  }
   render(){
     var hLevel = this.props.toclevel + 1;
     var hMethod = React.DOM['h' + hLevel];
     var headingText = <span dangerouslySetInnerHTML={{ __html: this.props.line}} />
-    var heading = hMethod.call(React.DOM, {}, headingText);
+    var icon = <Icon glyph="arrow" className="indicator" />
+    var heading = hMethod.call(React.DOM, { onClick: this.onToggle.bind(this) }, [ icon, headingText ] );
     return (
-      <div>
+      <div className={ this.state.isOpen ? 'section open-block' : 'section' }>
         {heading}
         <SectionContent {...this.props} text={this.props.text}></SectionContent>
       </div>
