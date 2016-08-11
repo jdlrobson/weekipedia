10513898ce94f24d5b9a6d66c123def53f2833ee
jdlrobson <jdlrobson@gmail.com>
2016-08-11 16:06:47 -0700

    Support list mode of mediawiki api and fetch headers
    There are various queries that will need these (when
    authorization is required and many of the mediawiki api list
    queries)

--- libs/server/endpoints/mwApi.js
+++ libs/server/endpoints/mwApi.js
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-fetch'
 import param from 'node-jquery-param'
 
-export default function ( lang, params, project ) {
+export default function ( lang, params, project, options ) {
   var baseParams = {
     action: 'query',
     format: 'json',
@@ -12,8 +12,7 @@ export default function ( lang, params, project ) {
   var url = 'https://' + lang + '.' + project + '.org/w/api.php?' +
     param( Object.assign( {}, baseParams, params ) );
 
-    console.log(url);
-  return fetch( url )
+  return fetch( url, options )
     .then( function ( resp ) {
      if ( resp.status === 200 ) {
        return resp.json();
@@ -24,8 +23,10 @@ export default function ( lang, params, project ) {
     .then( function ( json ) {
       if ( json.query && json.query.pages ) {
         return json.query.pages;
-      } else if ( params.meta ){
+      } else if ( params.meta ) {
         return json.query[params.meta];
+      } else if ( params.list ) {
+        return json;
       } else {
         return [];
       }
