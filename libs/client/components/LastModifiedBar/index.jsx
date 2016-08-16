3a6834691d7d97e0b1a6833a4adf90ad64d967a6
jdlrobson <jdlrobson@gmail.com>
2016-08-16 13:59:42 -0700

    Last modified bar is green when article has been edited in less than hour

--- libs/client/components/LastModifiedBar/index.jsx
+++ libs/client/components/LastModifiedBar/index.jsx
@@ -7,18 +7,28 @@ export default ({ editor, lang, title, timestamp }) => {
   const MOBILE_WP = 'https://' + lang + '.m.wikipedia.org/wiki/'
   const historyUrl = MOBILE_WP + 'Special:History/' + title
   const prefix = '/wiki/User:'
-  const historyText = 'Last edited ' + timeago(new Date(timestamp))
+  const tsAsDate = new Date( timestamp );
+  const historyText = 'Last edited ' + timeago( tsAsDate )
+  const now = Math.round( new Date().getTime() / 1000 );
+  // calculate delta in seconds
+  const timeDelta = now - ( tsAsDate.getTime() / 1000 );
 
-  var editorElement, text = '', editorLabel;
+  var editorElement, text = '', editorLabel,
+    className = 'last-modified-bar';
+
   // Cached pages may not have this available.
   if ( editor ) {
     editorLabel = isAnon ? 'an anonymous user' : editor.name
     editorElement = isAnon ? <span>{editorLabel}</span> : <a href={prefix + editor.name}>{editorLabel}</a>
     text = ' by ';
   }
+  // check if fresh (< 1hr)
+  if ( timeDelta < 60 * 60 ) {
+    className += ' active';
+  }
 
   return (
-    <div className="last-modified-bar">
+    <div className={className}>
       <div>
         <a href={historyUrl}>{historyText}</a>{text}{editorElement}
       </div>
