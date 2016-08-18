bcc85b0fac2bf39e0724556597e68c797fcd42d0
jdlrobson <jdlrobson@gmail.com>
2016-08-18 10:41:31 -0700

    Wait 40 minutes before attempting a purge

--- libs/server/collection.js
+++ libs/server/collection.js
@@ -4,7 +4,7 @@ const project = process.env.PROJECT || 'wikipedia';
 const collection = new WikiSocketCollection( {
   id: 'mysocket',
   project: '*.' + project + '.org',
-  minPurgeTime: 20,
+  minPurgeTime: 40,
   maxLifespan: ( 60 * 24 ) * 7,
   maxInactivity: ( 60 * 24 ) * 7,
   minSpeed: 0.1
