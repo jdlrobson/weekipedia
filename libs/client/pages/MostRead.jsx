d672f436c083383214700a9ad7fea495c2093d19
jdlrobson <jdlrobson@gmail.com>
2016-08-19 15:51:38 -0700

    Cleanup redundant code in MostRead

--- libs/client/pages/MostRead.jsx
+++ libs/client/pages/MostRead.jsx
@@ -1,34 +1,14 @@
 import React from 'react'
 
-import ErrorBox from './../components/ErrorBox';
-
-import Content from './../containers/Content'
-
 import CardListPage from './CardListPage'
 
 // Pages
 export default React.createClass({
-  getDefaultProps: function () {
-    return {
-      api: null
-    };
-  },
-  getInitialState() {
-    return {
-      error: false
-    };
-  },
   render(){
-    if ( this.state.error ) {
-      return (<Content><ErrorBox msg="Something went wrong when trying to get most read."></ErrorBox></Content>)
-    } else {
-      var endpoint = '/api/visits/' + this.props.lang;
-
-      return (
-        <CardListPage {...this.props} apiEndpoint={endpoint}
-          title='Most read' tagline="Pages that others are reading" />
-      )
-    }
+    return (
+      <CardListPage {...this.props} apiEndpoint={'/api/visits/' + this.props.lang}
+        title='Most read' tagline="Pages that others are reading" />
+    )
   }
 })
 
