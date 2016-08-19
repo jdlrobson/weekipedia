jdlrobson/weekipedia

commit 8e1a83ec836261297e27a7a9bfd1f408fa0653a8

Allow users to tab between watchlist feed and list
Watchlist now uses tabs.

libs/client/pages/Watchlist/WatchlistFeed.jsx
@@ -1,0 +1,27 @@
+import React from 'react'
+
+import CardListPage from './../CardListPage'
+
+// Pages
+export default React.createClass({
+  render() {
+    var ns, match;
+    var endpoint = '/api/private/watchlist-feed/' + this.props.lang + '/';
+
+    if ( window.location.search.indexOf( 'filter=' ) ) {
+      match = window.location.search.match( /filter\=([0-9])/ );
+      if ( match ) {
+        ns = match[1];
+      }
+    }
+
+    if ( ns ) {
+      endpoint += '/' + ns;
+    }
+
+    return (
+      <CardListPage {...this.props} unordered="1" apiEndpoint={endpoint} />
+    )
+  }
+})
+

libs/client/pages/Watchlist/index.jsx
@@ -1,0 +1,33 @@
+import React from 'react'
+
+import WatchListEdit from './WatchListEdit'
+import WatchListFeed from './WatchListFeed'
+
+// Pages
+export default React.createClass({
+  render(){
+    var props = this.props;
+    var isEditWatchlist = props.title === 'EditWatchlist';
+    var prefix = '/' + this.props.lang + '/wiki/';
+
+    var editLink = <a
+      className={isEditWatchlist ? 'active' : '' }
+      href={prefix + 'Special:EditWatchlist'}>List</a>;
+
+    var modLink = <a
+      className={isEditWatchlist ? '' : 'active' }
+      href={prefix + 'Special:Watchlist'}>Modified</a>;
+
+    var newProps = Object.assign( {}, props, {
+      tabs: [ editLink, modLink ],
+      tagline: 'Pages on your watchlist',
+      title: 'Watchlist'
+    } );
+
+    if ( isEditWatchlist ) {
+      return (<WatchListEdit {...newProps} />);
+    } else {
+      return (<WatchListFeed {...newProps} />);
+    }
+  }
+})

libs/client/pages/WatchlistFeed.jsx
@@ -1,18 +1,0 @@
-import React from 'react'
-
-import CardListPage from './CardListPage'
-
-import DiffCard from './../components/CardDiff'
-
-// Pages
-export default React.createClass({
-  render(){
-    var endpoint = '/api/private/watchlist-feed/' + this.props.lang + '/';
-
-    return (
-      <CardListPage {...this.props} apiEndpoint={endpoint} isDiffCardList={true}
-        title="Watchlist" CardClass={DiffCard} />
-    )
-  }
-})
-

libs/client/pages/Watchlist.jsx
@@ -1,14 +1,0 @@
-import React from 'react'
-
-import CardListPage from './CardListPage'
-
-// Pages
-export default React.createClass({
-  render(){
-    return (
-      <CardListPage {...this.props} unordered="1" apiEndpoint={'/api/private/watchlist/' + this.props.lang}
-        title='Watchlist' tagline="Pages on your watchlist" />
-    )
-  }
-})
-

libs/client/pages/Watchlist/WatchlistEdit.jsx
@@ -1,0 +1,13 @@
+import React from 'react'
+
+import CardListPage from './../CardListPage'
+
+// Pages
+export default React.createClass({
+  render(){
+    return (
+      <CardListPage {...this.props} unordered="1" apiEndpoint={'/api/private/watchlist/' + this.props.lang} />
+    )
+  }
+})
+

libs/client/routes.js
@@ -10,7 +10,6 @@ import MostRead from './pages/MostRead'
 import Random from './pages/Random'
 import Nearby from './pages/Nearby'
 import Watchlist from './pages/Watchlist'
-import WatchlistFeed from './pages/WatchlistFeed'
 
 import utils from './utils'
 
@@ -87,6 +86,7 @@ function addSpecialPage( title, Class, handler ) {
       props.children = [
         React.createElement( Class,
           Object.assign( {}, props, {
+            title: title,
             key: 'page-special-' + title,
             params: info[3] || info[2]
           } )
@@ -102,7 +102,7 @@ function initSpecialPages() {
   addSpecialPage( 'History', History );
   addSpecialPage( 'RecentChanges', Contributions );
   addSpecialPage( 'Contributions', Contributions );
-  addSpecialPage( 'Watchlist', WatchlistFeed );
+  addSpecialPage( 'Watchlist', Watchlist );
   addSpecialPage( 'EditWatchlist', Watchlist );
   addSpecialPage( 'MostRead', MostRead );
   addSpecialPage( 'SplashScreen', SplashScreen );

commit c52232669f76e654d846f3dfbe553a3bcd2360b0

Fix watchlist feed view
It's unordered and it's a diff card list
Make sure anything with isDiffCardList set is also unordered.

Broken in 8e1a83e

libs/client/containers/CardList/index.jsx
@@ -64,13 +64,15 @@ export default React.createClass({
   render: function () {
     var lastTs;
     var props = this.props;
+    var isDiffCardList = this.props.isDiffCardList;
+    var isUnordered = props.unordered && !isDiffCardList;
     var cards = this.state.cards;
     var cardsAndHeaders = [];
     var continuer = props.continue && props.endpoint ?
       <div className='gutter' /> : null;
-    var className = 'card-list' + ( props.unordered ? ' card-list-unordered' : '' );
+    var className = 'card-list' + ( isUnordered ? ' card-list-unordered' : '' );
 
-    if ( this.props.isDiffCardList ) {
+    if ( isDiffCardList ) {
       className += ' diff-list';
       cards.forEach( function ( card, i ) {
         var ts, header;

libs/client/pages/Watchlist/WatchlistFeed.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import DiffCard from './../../components/CardDiff'
+
 import CardListPage from './../CardListPage'
 
 // Pages
@@ -20,7 +22,8 @@ export default React.createClass({
     }
 
     return (
-      <CardListPage {...this.props} unordered="1" apiEndpoint={endpoint} />
+      <CardListPage {...this.props} apiEndpoint={endpoint}
+        CardClass={DiffCard} isDiffCardList={true} />
     )
   }
 })