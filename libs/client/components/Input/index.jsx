128f4a8c5c109429d5cf53656084b4cce8c9aee3
jdlrobson <jdlrobson@gmail.com>
2016-08-24 13:33:15 -0700

    Add Input component

---
+++ libs/client/components/Input/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './mediawiki-ui-input.css'
+
+const Input = (props) => {
+  var suffix = props.className ? ' ' + props.className : '';
+  var inputProps = {
+    className: 'mw-ui-input' + suffix,
+    placeholder: props.placeholder,
+    onInput: props.onInput,
+    defaultValue: props.defaultValue
+  };
+
+  if ( props.textarea ) {
+    return <textarea {...inputProps} />;
+  } else {
+    return <input {...inputProps} />
+  }
+}
+
+export default Input
