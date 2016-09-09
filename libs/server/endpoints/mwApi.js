13e8be6d913376736a64c5126eb7a1a15ca6707c
jdlrobson <jdlrobson@gmail.com>
2016-09-09 13:20:03 -0700

    Use oauth-fetch-json npm module

--- libs/server/endpoints/mwApi.js
+++ libs/server/endpoints/mwApi.js
@@ -1,6 +1,4 @@
-import fetch from 'isomorphic-fetch'
-import param from 'node-jquery-param'
-import OAuth from 'oauth'
+import oauthFetchJson from 'oauth-fetch-json'
 
 function flatten( pages ) {
   pages.forEach( function ( page ) {
@@ -13,64 +11,6 @@ function flatten( pages ) {
     }
   } );
   return pages;
-}
-
-function signedRequest( url, session, params, options ) {
-  var oauth = new OAuth.OAuth(
-    // URL to request a token
-    'https://meta.wikimedia.org/index.php?title=Special%3AOAuth%2Finitiate',
-    // URL to get access token
-    'https://meta.wikimedia.org/index.php??title=Special%3AOAuth%2Ftoken',
-    session.oauth.consumer_key,
-    session.oauth.consumer_secret,
-    '1.0',
-    null,
-    'HMAC-SHA1'
-  );
-
-  return new Promise( function ( resolve, reject ) {
-    var handler = function ( err, data ) {
-      if ( err ) {
-        reject( JSON.stringify( err ) );
-      } else {
-        resolve( JSON.parse( data ) );
-      }
-    };
-
-    if ( options && options.method === 'POST' ) {
-      oauth.post(
-        url,
-        session.oauth.token,
-        session.oauth.token_secret,
-        params,
-        handler );
-    } else {
-      url += '?' + param( params );
-      oauth.get(
-        url,
-        session.oauth.token,
-        session.oauth.token_secret,
-        handler);
-    }
-  } );
-}
-
-function anonRequest( url, params, options ) {
-  if ( options && options.method === 'POST' ) {
-    options.body = JSON.stringify( params );
-  } else {
-    url += '?' + param( Object.assign( {}, params ) );
-  }
-  return fetch( url, options )
-    .then( function ( resp ) {
-     if ( resp.status === 200 ) {
-       return resp.json();
-     } else {
-       throw Error( resp.status );
-     }
-    } ).then( function ( json ) {
-      return json;
-    });
 }
 
 export default function ( lang, params, project, options, session ) {
@@ -87,13 +27,7 @@ export default function ( lang, params, project, options, session ) {
 
   fullParams = Object.assign( {}, baseParams, params );
 
-  if ( session ) {
-    req = signedRequest( url, session, fullParams, options );
-  } else {
-    req = anonRequest( url, fullParams, options );
-  }
-
-  return req.then( function ( json ) {
+  return oauthFetchJson( url, fullParams, options, session ? session.oauth : null ).then( function ( json ) {
     if ( json.error ) {
       throw new Error( json.error.code + ': ' + json.error.info );
     } else if ( json.query && json.query.pages ) {
