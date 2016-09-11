9f6365e1e21ce47dd4e0de021457f2b0731163e1
jdlrobson <jdlrobson@gmail.com>
2016-09-11 10:05:45 -0700

    All Icon's should be blank if they require client-side JavaScript to work
    In preparation for server side rendering we should not show icons
    which have no function

--- libs/client/components/Icon/index.jsx
+++ libs/client/components/Icon/index.jsx
@@ -2,21 +2,36 @@ import React from 'react'
 
 import './mediawiki-ui-icon.css'
 
-const Icon = (props) => {
-  var iconProps = {
-    className: (props.className || '') + ' mw-ui-icon mw-ui-icon-' + props.glyph + ' mw-ui-icon-' +
-      ( props.type || 'element' ),
-    href: props.href,
-    onClick: props.onClick
-  };
-  if ( props.small ) {
-    iconProps.className += ' mw-ui-icon-small';
-  } else if ( props.large ) {
-    iconProps.className += ' mw-ui-icon-large';
+export default React.createClass({
+  componentWillMount() {
+    this.setState( { jsEnabled: false } );
+  },
+  componentDidMount() {
+    this.setState( { jsEnabled: true } );
+  },
+  render() {
+    var props = this.props;
+    var glyph = props.glyph;
+    // If the icon's only purpose is to run with JS and JS is not available it should be disabled
+    if ( !props.href && props.onClick && !this.state.jsEnabled ) {
+      glyph = '';
+    } else if ( props.href && props.href.indexOf( '#/' ) === 0 ) {
+      glyph = '';
+    }
+
+    var iconProps = {
+      className: (props.className || '') + ' mw-ui-icon mw-ui-icon-' + glyph + ' mw-ui-icon-' +
+        ( props.type || 'element' ),
+      href: props.href,
+      onClick: props.onClick
+    };
+    if ( props.small ) {
+      iconProps.className += ' mw-ui-icon-small';
+    } else if ( props.large ) {
+      iconProps.className += ' mw-ui-icon-large';
+    }
+
+    return iconProps.href ? <a {...iconProps}>{props.label}</a> :
+      <div {...iconProps}>{props.label}</div>;
   }
-
-  return iconProps.href ? <a {...iconProps}>{props.label}</a> :
-    <div {...iconProps}>{props.label}</div>;
-}
-
-export default Icon
+});
