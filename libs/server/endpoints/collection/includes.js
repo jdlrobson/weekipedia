85c9c25f8c809328710b01263176ba8339cea6b4
jdlrobson <jdlrobson@gmail.com>
2016-09-17 17:28:20 -0700

    Refactor collection list and includes endpoints
    Up limit of collections up to 500.

--- libs/server/endpoints/collection/includes.js
+++ libs/server/endpoints/collection/includes.js
@@ -1,37 +1,9 @@
-import mwApi from './../mwApi'
-import extractInfo from './extract-info'
-import extractMembers from './extract-members'
+import list from './list'
 import watched from './../watched'
 
 function includes( lang, project, title, profile ) {
   var username = profile.displayName;
-  var params = {
-    prop: 'revisions',
-    generator: 'prefixsearch',
-    rvprop: 'content|timestamp',
-    gpssearch: 'User:' + username + '/lists/',
-    gpsnamespace: 2
-  }
-
-  return mwApi( lang, params, project ).then( function ( json ) {
-    var result = { collections: [] };
-    json.pages.forEach( function ( page ) {
-      var split, members, info,
-        revs = page.revisions;
-
-      if ( revs[0] ) {
-        split = revs[0].content.split( '==\n' );
-        members = split[1] ? extractMembers( split[1] ) : [];
-        info = extractInfo( page.title, split[0], revs[0].timestamp );
-        info.member = members.indexOf( title ) > -1;
-        if ( info.id !== 0 ) {
-          result.collections.push( info );
-        }
-      }
-    } );
-    result.collections = result.collections.sort( function ( a, b ) {
-      return a.updated > b.updated ? -1 : 1;
-    } );
+  return list( lang, project, username, title ).then( function ( result ) {
     if ( profile ) {
       return watched( lang, project, [ title ], profile ).then( function ( watchInfo ) {
         result.collections.unshift( {
--- libs/server/endpoints/collection/list.js
+++ libs/server/endpoints/collection/list.js
@@ -1,30 +1,47 @@
 import mwApi from './../mwApi'
 import extractInfo from './extract-info'
+import extractMembers from './extract-members'
 import thumbFromTitle from './thumbnail-from-title.js'
 
-function list( lang, project, username ) {
+function list( lang, project, username, title ) {
   var params = {
     prop: 'revisions|images',
     generator: 'prefixsearch',
     rvprop: 'content|timestamp',
-    rvsection: 0,
+    gpslimit: 500,
     gpssearch: 'User:' + username + '/lists/',
     gpsnamespace: 2
+  };
+  if ( !title ) {
+    params.rvsection = 0;
   }
 
   return mwApi( lang, params, project ).then( function ( json ) {
     var result = { collections: [] };
     json.pages.forEach( function ( page ) {
-      var collection,
+      var collection, split, head,
+        members = [],
         revs = page.revisions;
 
       if ( revs[0] ) {
-        collection = extractInfo( page.title, revs[0].content, revs[0].timestamp );
+        head = revs[0].content;
+        if ( title ) {
+          split = head.split( '==\n' );
+          members = split[1] ? extractMembers( split[1] ) : [];
+          head = split[0];
+        }
+
+        collection = extractInfo( page.title, head, revs[0].timestamp );
+        if ( title ) {
+          collection.member = members.indexOf( title ) > -1;
+        }
+
         if ( page.images ) {
           collection.thumbnail = {
             source: thumbFromTitle( page.images[0].title.split( ':' )[1], 200 )
           };
         }
+
         // id=0 is reserved for watchlist so don't treat it as collection
         if ( collection.id !== 0 ) {
           result.collections.push( collection );
