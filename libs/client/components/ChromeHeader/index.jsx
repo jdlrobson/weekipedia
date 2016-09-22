79e177b145c4a430d9b351186e10524cc8a0eefa
jdlrobson <jdlrobson@gmail.com>
2016-09-22 08:44:00 -0700

    Show current project in header if not default wiki
    To give a bit more of a clue about what's going on

--- libs/client/components/ChromeHeader/index.jsx
+++ libs/client/components/ChromeHeader/index.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 
+import './styles.less'
+
 class ChromeHeader extends Component {
   render(){
+    var icon;
     var siteinfo = this.props.siteinfo;
     var content = siteinfo.wordmark ? <img src={siteinfo.wordmark} alt={siteinfo.title} height="18" /> :
       siteinfo.title;
+    var project = this.props.project;
 
+    if ( project !== siteinfo.defaultProject ) {
+      icon = <div className={"project-icon project-" + project}>{project}</div>
+    }
     return (
-      <h1>{content}</h1>
+      <h1>{content} {icon}</h1>
     )
   }
 }
