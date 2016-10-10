8c65a99cb80496d0d9b3d139e847e10a6a0098fc
jdlrobson <jdlrobson@gmail.com>
2016-10-10 12:32:14 -0700

    Correct server side rendering
    `session` is a library that is never null so this is a dead code path.
    This fixes that.

--- libs/server/app.js
+++ libs/server/app.js
@@ -231,7 +231,10 @@ app.get( '/:lang?/*', ( req, res ) => {
       page_title: title,
       isRTL: isRTL( req.params.lang ),
       config: JSON.stringify( config ),
-      body: !session && SERVER_SIDE_RENDERING ? ReactDOMServer.renderToString( shared.render( req.path, '#', data, req.query ) ) : ''
+      body: !req.user && SERVER_SIDE_RENDERING ?
+        ReactDOMServer.renderToString(
+          shared.render( req.path, '#', data, req.query )
+        ) : ''
     } );
   }
   if ( route.fallback && SERVER_SIDE_RENDERING ) {
