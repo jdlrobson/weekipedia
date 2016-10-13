8a41513f254c1e11dc6f211d6604807678bca42e
jdlrobson <jdlrobson@gmail.com>
2016-10-13 14:09:57 -0700

    Render RecentChanges consistently with production
    Use a paragraph rather than tagline.
    
    Fixes: #105

--- libs/client/pages/Contributions.jsx
+++ libs/client/pages/Contributions.jsx
@@ -8,7 +8,7 @@ import Icon from './../components/Icon'
 // Pages
 export default React.createClass({
   render(){
-    var title, tagline,
+    var title, lead, tagline,
       source = this.props.language_project || this.props.lang,
       endpoint = '/api/contributions/' + source + '/0/',
       username = this.props.params;
@@ -26,12 +26,14 @@ export default React.createClass({
       endpoint += username;
     } else {
       title = 'Recent Changes';
-      tagline = 'This is a list of recent changes to Wikipedia';
+      lead = {
+        paragraph: 'This is a list of recent changes to Wikipedia'
+      };
     }
 
     return (
       <CardListPage {...this.props} apiEndpoint={endpoint} isDiffCardList={true}
-        title={title} tagline={tagline} CardClass={CardDiff} />
+        title={title} tagline={tagline} CardClass={CardDiff} lead={lead} />
     )
   }
 })
