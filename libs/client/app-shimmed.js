cd108bd2c34a84fc59a82e2da94c1cf93da5e354
jdlrobson <jdlrobson@gmail.com>
2016-10-15 12:31:40 -0700

    jscs fix follow up

--- libs/client/app-shimmed.js
+++ libs/client/app-shimmed.js
@@ -1,8 +1,8 @@
 // setup polyfills
-require('phantomjs-polyfill');
-require('es6-map/implement');
-require('phantomjs-polyfill-object-assign');
-window.Promise = require('es6-promise-polyfill').Promise;
+require( 'phantomjs-polyfill' );
+require( 'es6-map/implement' );
+require( 'phantomjs-polyfill-object-assign' );
+window.Promise = require( 'es6-promise-polyfill' ).Promise;
 
 // run like normal
 require( './app' );
