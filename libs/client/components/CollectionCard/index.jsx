1d3461a97fecf2471bcbda18ab53bab2d5a201d0
jdlrobson <jdlrobson@gmail.com>
2016-12-02 18:33:53 -0800

    Collection cards should show owner information

--- libs/client/components/CollectionCard/index.jsx
+++ libs/client/components/CollectionCard/index.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
-import { CardWithLocation } from 'wikipedia-react-components'
+import { Card, Icon } from 'wikipedia-react-components'
 
 class CollectionCard extends Component {
   render(){
     var props = this.props;
-    var url = '/' + props.lang + '/wiki/Special:Collections/by/' + props.owner + '/' + props.id;
-    return <CardWithLocation {...props} url={url} />;
+    var owner = props.owner;
+    var base = '/' + props.language_project + '/';
+    var url = base + 'Special:Collections/by/' + props.owner + '/' + props.id;
+    var extracts = [
+      props.description
+    ];
+    if ( owner ) {
+      extracts.push( <Icon glyph="user" type="before" label={owner} className="mw-mf-user"
+        href={base + 'User:' + encodeURIComponent( owner )} /> );
+    }
+    return <Card {...props} url={url} extracts={extracts} />;
   }
 }
 
