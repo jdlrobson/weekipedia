467bcab81f6cf29e75a41c71637a593287fed4fa
jdlrobson <jdlrobson@gmail.com>
2016-12-04 09:51:46 -0800

    When only one project available search page should not show other project
    If the option allowForeignProjects is disabled do not show the complicated
    header on the search page as the links will not work
    
    Also link to the term itself in case the page does not exist

--- libs/client/pages/Search.jsx
+++ libs/client/pages/Search.jsx
@@ -20,24 +20,28 @@ export default React.createClass({
   getTabs() {
     var proj = this.props.project;
     var term = this.getTerm();
+    var siteoptions = this.props.siteoptions;
     var self = this;
     var prefix = '/' + this.props.lang + '.';
-    return this.props.supportedProjects.map( function ( project, i ) {
+    return siteoptions.allowForeignProjects ? this.props.supportedProjects.map( function ( project, i ) {
       return <a key={'search-tab' + i}
         onClick={self.navigateTo}
         className={proj === project ? 'active' : ''}
         title={'Search ' + project + ' for ' + term}
         href={prefix + project + '/Special:Search/' + term}>{project}</a>
-    } );
+    } ) : [];
   },
   render(){
     var emptyProps = {
       msg: 'No pages matched your search query for this project. Why not try one of our other projects?'
     };
     var props = this.props;
+    var siteoptions = props.siteoptions;
     var term = this.getTerm();
     var endpoint = '/api/search-full/' + props.language_project + '/' + encodeURIComponent( term );
-    var tagline = <p>Showing you all search results for <strong>{decodeURIComponent(term)}</strong> on <strong>{props.project}</strong></p>;
+    var suffix = siteoptions.allowForeignProjects ? [ ' on ', <strong>{props.project}</strong> ] : '';
+    var termUrl = '/' + props.language_project + '/' + term;
+    var tagline = <p>Showing you all search results for <strong><a href={termUrl}>{decodeURIComponent(term)}</a></strong>{suffix}</p>;
 
     // mw-search-results class added for consistency with MediaWiki
     return (
