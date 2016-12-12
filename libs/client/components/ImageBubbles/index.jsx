1dc3da51c6f6b620a483a9dc5d5451df019d66b7
jdlrobson <jdlrobson@gmail.com>
2016-12-11 17:52:27 -0800

    Fix what happens when image bubbles are clicked

--- libs/client/components/ImageBubbles/index.jsx
+++ libs/client/components/ImageBubbles/index.jsx
@@ -6,7 +6,9 @@ export default React.createClass({
   onClick( ev ) {
     ev.preventDefault();
     this.props.router.navigateTo( '#/media/' +
-      ev.currentTarget.getAttribute( 'href' ).replace( '/wiki/File:', '' ) );
+      ev.currentTarget.getAttribute( 'href' ).replace( '/wiki/File:', '' )
+       .replace( './File:', '' )
+    );
   },
   render() {
     var props = this.props;
