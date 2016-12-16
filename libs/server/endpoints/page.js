e83c98770cd73409be5e8eb9d06dc293c7a96c71
jdlrobson <jdlrobson@gmail.com>
2016-12-16 10:09:04 -0800

    Encode forward clashes in links where they mean page titles
    For example in Central london section of WIkivoyage
    http://localhost:8142/wiki/London the rewrites result in bad links

--- libs/server/endpoints/page.js
+++ libs/server/endpoints/page.js
@@ -84,14 +84,20 @@ function extractPageIssues( doc ) {
 
 // Undo the work in mobile-content-service (https://phabricator.wikimedia.org/T147043)
 function undoLinkRewrite( doc ) {
-  var idx = 0;
+  var idx = 0, sp;
   var ps = doc.querySelectorAll( 'a' ) || [],
       value;
   for ( idx = 0; idx < ps.length; idx++ ) {
     var node = ps[idx];
     value = node.getAttribute( 'href' );
     if ( value ) {
+      // replace all subpages with encoded '/'
       value = value.replace( /^\/wiki\//, './' );
+      if ( value.substr(0, 2) === './' ) {
+        sp = value.substr(2);
+        sp = sp.replace( /\//g, '%2F' );
+        value = './' + sp;
+      }
       node.setAttribute( 'href', value );
     }
   }
