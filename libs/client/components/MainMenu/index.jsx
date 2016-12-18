ae18fcd190c6a63411a1f51e8d65874707b7ebac
jdlrobson <jdlrobson@gmail.com>
2016-12-18 15:16:56 -0800

    Fix main menu icon names/colors for consistency

--- libs/client/components/MainMenu/index.jsx
+++ libs/client/components/MainMenu/index.jsx
@@ -35,25 +35,25 @@ class MainMenu extends Component {
       if ( this.props.session ) {
         username = this.props.session.username;
         login = [
-          <Icon glyph="mf-profile-invert" href={prefix + 'User:' + username }
+          <Icon glyph="mf-profile" href={prefix + 'User:' + username }
             key="menu-item-profile"
            label={username} type="before" onClick={onMenuItemClick} />,
-          <Icon glyph="mf-logout-invert" href='/auth/logout'
+          <Icon glyph="mf-logout" href='/auth/logout'
             key="menu-item-logout"
             label={msg('menu-logout')} onClick={this.onLogoutClick.bind(this)} />
         ];
         usertools = [
           <li key="menu-item-watchlist">
-            <Icon glyph="mf-watchlist-invert" href={prefix + 'Special:Watchlist' }
+            <Icon glyph="mf-watchlist" href={prefix + 'Special:Watchlist' }
               label={msg('menu-watchlist')} type="before" onClick={onMenuItemClick} />
           </li>,
           <li key="menu-item-contribs">
-            <Icon glyph="mf-contributions-invert" href={prefix + 'Special:Contributions/' + username }
+            <Icon glyph="mf-contributions" href={prefix + 'Special:Contributions/' + username }
               label={msg('menu-contributions')} type="before" onClick={onMenuItemClick} />
           </li>
         ];
       } else {
-        login = <Icon glyph="mf-anonymous-invert"
+        login = <Icon glyph="mf-anonymous"
           href={prefix + 'Special:UserLogin?returnto=' + props.title}
           label={msg('menu-login')} type="before" onClick={this.onLoginClick.bind(this)} />;
       }
@@ -80,7 +80,7 @@ class MainMenu extends Component {
     if ( options.collectionsEnabled ) {
       collectionMenuItem = (
         <li>
-          <Icon glyph="mf-collections-invert" href={'/' + this.props.lang + '/wiki/Special:Collections/' }
+          <Icon glyph="mf-collections" href={'/' + this.props.lang + '/wiki/Special:Collections/' }
             label={msg('menu-collections')} type="before" onClick={onMenuItemClick} />
         </li>
       );
@@ -88,7 +88,7 @@ class MainMenu extends Component {
     if ( options.nearby ) {
       nearbyMenuItem = (
         <li>
-          <Icon glyph="mf-nearby-invert" href={langPrefix + '/wiki/Special:Nearby'}
+          <Icon glyph="mf-nearby" href={langPrefix + '/wiki/Special:Nearby'}
             onClick={onMenuItemClick}
             label={msg('menu-nearby')} type="before"/>
         </li>
@@ -97,7 +97,7 @@ class MainMenu extends Component {
     if ( options.settingsEnabled ) {
         settingsMenuItem = (
           <li>
-            <Icon glyph="mf-settings-invert" href={'/' + this.props.lang + '/wiki/Special:MobileOptions' }
+            <Icon glyph="mf-settings" href={'/' + this.props.lang + '/wiki/Special:MobileOptions' }
               label={msg('menu-settings')} type="before" onClick={onMenuItemClick} />
           </li>
         );
@@ -106,11 +106,11 @@ class MainMenu extends Component {
       <div className="component-main-menu menu">
         <ul>
           <li>
-            <Icon glyph="mf-home-invert" href="/" label={msg('menu-home')} type="before"
+            <Icon glyph="mf-home" href="/" label={msg('menu-home')} type="before"
               onClick={onMenuItemClick} />
           </li>
           <li>
-            <Icon glyph="mf-random-invert" href={langPrefix + '/wiki/Special:Random'}
+            <Icon glyph="mf-random" href={langPrefix + '/wiki/Special:Random'}
               onClick={onMenuItemClick}
               label={msg('menu-random')} type="before"/>
           </li>
