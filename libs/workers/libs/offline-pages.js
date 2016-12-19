9e12d4e19513480c6e92b67dca9ea7db10addfe9
jdlrobson <jdlrobson@gmail.com>
2016-12-20 06:28:29 +0900

    Account for unexpected fetch responses
    e.g. 404s do not have a lead

--- libs/workers/libs/offline-pages.js
+++ libs/workers/libs/offline-pages.js
@@ -7,25 +7,27 @@ function offlinePages( cache ) {
   return offlineRequests( cache ).then( ( reqs ) => {
     reqs.forEach( ( item ) => {
       var lead = item.content.lead;
-      var page = {
-        title: lead.displaytitle,
-        description: lead.description,
-        modified: item.response.headers.get( 'date' ),
-        coordinates: lead.coordinates,
-        _key: item.request
-      };
-
-      if ( lead.image ) {
-        page.thumbnail = {
-          source: lead.image.urls[320],
-          width: 320
+      if ( lead ) {
+        var page = {
+          title: lead.displaytitle,
+          description: lead.description,
+          modified: item.response.headers.get( 'date' ),
+          coordinates: lead.coordinates,
+          _key: item.request
         };
-      }
-      // Only surface pages with coordinates and in main namespace
-      if ( lead.ns === 0 && lead.coordinates && !members[page.title] ) {
-        // Don't repeat pages
-        members[page.title] = true;
-        pages.push( page );
+
+        if ( lead.image ) {
+          page.thumbnail = {
+            source: lead.image.urls[320],
+            width: 320
+          };
+        }
+        // Only surface pages with coordinates and in main namespace
+        if ( lead.ns === 0 && lead.coordinates && !members[page.title] ) {
+          // Don't repeat pages
+          members[page.title] = true;
+          pages.push( page );
+        }
       }
     } );
     return pages;
