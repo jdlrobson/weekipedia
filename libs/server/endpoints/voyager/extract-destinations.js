0e77bcf9ce467501913965270203ae96848a86e4
jdlrobson <jdlrobson@gmail.com>
2017-02-04 13:31:44 -0800

    Fix bug in getParentWithTag
    A node may not have any parents if you search to the top of the
    document

--- libs/server/endpoints/voyager/extract-destinations.js
+++ libs/server/endpoints/voyager/extract-destinations.js
@@ -54,7 +54,9 @@ function extractDestinations( section ) {
 }
 
 function getParentWithTag( node, tag ) {
-  if ( node.tagName === tag ) {
+  if ( !node ) {
+    return false;
+  } else if ( node.tagName === tag ) {
     return node;
   } else {
     return getParentWithTag( node.parentNode, tag );
