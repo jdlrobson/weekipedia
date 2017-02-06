a4668f7402423386045a965ddd37f410d9146278
jdlrobson <jdlrobson@gmail.com>
2017-02-05 22:01:19 -0800

    Pages include revision in key

--- libs/shared/routes.js
+++ libs/shared/routes.js
@@ -93,7 +93,7 @@ var routes = [
         props.children = [
           React.createElement( View,
             Object.assign( {}, props, {
-              key: 'page-' + title,
+              key: 'page-' + title + '-' + props.revision,
               titleSansPrefix: titleSansPrefix,
               title: titleDecoded
             } )
