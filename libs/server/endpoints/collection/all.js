225c4ba0fe30c72c34b5bcc93f557113b05f7af2
jdlrobson <jdlrobson@gmail.com>
2018-09-28 16:53:16 -0700

    Remove local copyof collection endpoint for npm version
    Fixes: #157

--- libs/server/endpoints/collection/all.js
+++
@@ -1,7 +0,0 @@
-import list from './list';
-
-function all( lang, project, query ) {
-	return list( lang, project, null, null, query );
-}
-
-export default all;
--- libs/server/endpoints/collection/lookup.js
+++
@@ -1,7 +0,0 @@
-export default function ( username, collection ) {
-	var title = 'User:' + username + '/lists/';
-	if ( collection !== undefined ) {
-		title += collection;
-	}
-	return title;
-}
